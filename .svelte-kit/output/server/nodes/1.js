da7447b1e417ee6920f4eb37cea556a6e33fc87d
Reeserj <82838264+reeserj@users.noreply.github.com>
2025-01-11 14:45:32 -0600

    Deploy to GitHub Pages

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.Cd-FJk5J.js","_app/immutable/chunks/scheduler.Dnz8ZsAA.js","_app/immutable/chunks/index.cD3UBkeq.js","_app/immutable/chunks/entry.BJeJ-UwJ.js"];
-export const stylesheets = [];
-export const fonts = [];
--- Readme.md
+++
@@ -1,9 +0,0 @@
-# A svelte Landing page to document my build process
-## Features 
-- A carosel of my builds
-- Integrate Convertkit
-- Markdown to HTML converter
-- Templates for blog posts
-- header
-- footer with link to my github and link to my twitter
-
